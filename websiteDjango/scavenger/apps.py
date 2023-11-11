745429e0e26c9a6b5c2f5760f36f248256168941
Garb <87356816+Garbledorf@users.noreply.github.com>
2023-11-11 09:39:27 -0800

    Delete websiteDjango directory
    switched to flask

--- websiteDjango/scavenger/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScavengerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "scavenger"
--- websiteDjango/scavenger/programLocation/bridge.py
+++
@@ -1,6 +0,0 @@
-from main import search
-
-
-
-def search_initialize(term):
-    result = search(term)
